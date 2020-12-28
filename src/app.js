798ce5acb6264350a98c950451fb839b8f450728
Sam Nicholson <samnicho@Sams-MacBook-Pro.local>
2020-12-28 14:53:47 +0000

    set up webpack prod build

--- src/app.js
+++ src/app.js
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
-import "react-dates/lib/css/_datepicker.css";
 import "react-dates/initialize";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
+import "react-dates/lib/css/_datepicker.css";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  watch: true,
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    },{
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
-}
+module.exports = (env) => {
+  const isProd = env === "production";
+
+  return {
+    watch: true,
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: "styles.css" })],
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: "css-loader", options: { sourceMap: true } },
+            { loader: "sass-loader", options: { sourceMap: true } },
+          ],
+        },
+      ],
+    },
+    devtool: isProd ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+    },
+  };
+};
